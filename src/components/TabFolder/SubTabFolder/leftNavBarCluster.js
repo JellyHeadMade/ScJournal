b24cfbcb088783fa87842797b15f92113e75d38f
JellyHead <JellyHeadM@outlook.com>
2022-07-02 22:41:52 -0400

    i have a plan started to get the inended behavior for the navbar. only set up the home tab (1/3) to have its styles switch from state, so next is to get a series of event handlers for clicks that should change the state based of what is clicked. once that is done  only thing left to do will be to get the transistions done and the spacers. both of which should be very similar to the way the drop tabs a re set up. only difference is i need to delay the transistion based off how long the part further out takes to complete. i also need to make  sure this time offset goes inverse when in the oppisite direction.

--- src/components/TabFolder/SubTabFolder/leftNavBarCluster.js
+++ src/components/TabFolder/SubTabFolder/leftNavBarCluster.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function LeftNavBarCluster(props) {
+
+    const [hideShow, setHideShow] = useState({show: false})
+    const [homeState, setHomeState] = useState({activeState: 'notActive', hiddenState: 'Hidden'})
+    const [contactState, setContactState] = useState({activeState: 'notActive', hiddenState: 'Hidden'})
+    const [createState, setCreateState] = useState({activeState: 'notActive', hiddenState: 'Hidden'})
+
+    // const onClickArrow = (e) => {
+    //     if ()
+    // }
 
     return (
         <div className='left-nav-cluster-container'>
             <div className='logo-tab'>
                 <img src='https://via.placeholder.com/40/ffffff.png'/>
             </div>
-            <div className='home-tab'>Home</div>
+            <div className={`home-tab__${homeState.hiddenState}__${homeState.activeState}`}>Home</div>
             <div className='contact-tab'>About/Contact</div>
             <div className='create-post-tab'>Create Post</div>
             <div className='arrow-tab'>Arrow</div>
