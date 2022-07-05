484864c5ded4aed19bde3490506eabcf6b5c651c
JellyHead <JellyHeadM@outlook.com>
2022-07-05 19:07:19 -0400

    I now have the intended behaviour for the navbar. by default it only shows the home tab, as its the default choice, in the active color, then if the arrow is clicked the other tabs come out. once out you can only click on a non-active tab, which will then turn the clicked on tab into the active one, with all the styles, and the other tabs go away. then this new tab active tab is unclickable. the tabs still need animation, and the rest of the styles but the hard functionality is done. i still have to restructure the web app so the nav components can actually change out what is shown below, and to maintain them on top. they will likely move up one more structure to be intergrated with Route. once that is done the right nav side is all that is left for the navigation.

--- src/components/TabFolder/SubTabFolder/leftNavBarCluster.js
+++ src/components/TabFolder/SubTabFolder/leftNavBarCluster.js
@@ -1,27 +1,84 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-function LeftNavBarCluster(props) {
+const HOME_TAB_STATE = [
+    {name: 'Home', active: true, clickable: false}, 
+    {name: 'Contact', active: false, clickable: true}, 
+    {name: 'Create', active: false, clickable: true}
+];
+const CONTACT_TAB_STATE = [
+    {name: 'Home', active: false, clickable: true}, 
+    {name: 'Contact', active: true, clickable: false}, 
+    {name: 'Create', active: false, clickable: true}
+];
+const CREATE_TAB_STATE = [
+    {name: 'Home', active: false, clickable: true}, 
+    {name: 'Contact', active: false, clickable: true}, 
+    {name: 'Create', active: true, clickable: false}
+];
 
-    const [hideShow, setHideShow] = useState({show: false})
-    const [homeState, setHomeState] = useState({activeState: 'notActive', hiddenState: 'Hidden'})
-    const [contactState, setContactState] = useState({activeState: 'notActive', hiddenState: 'Hidden'})
-    const [createState, setCreateState] = useState({activeState: 'notActive', hiddenState: 'Hidden'})
+class LeftNavBarCluster extends React.Component {
+    constructor(props) {
+        super(props);
 
-    // const onClickArrow = (e) => {
-    //     if ()
-    // }
+        this.state = {
+            HiddenState: 'Hidden',
+            activeStates: HOME_TAB_STATE
+        }
+        this.onClickArrow = this.onClickArrow.bind(this);
+        this.handleActiveClick = this.handleActiveClick.bind(this);
+    }
 
-    return (
-        <div className='left-nav-cluster-container'>
-            <div className='logo-tab'>
-                <img src='https://via.placeholder.com/40/ffffff.png'/>
+    onClickArrow(e) {
+        switch(this.state.HiddenState) {
+            case 'Hidden': 
+                this.setState({HiddenState: 'notHidden'});
+                break;
+            case 'notHidden':
+                this.setState({HiddenState: 'Hidden'});
+                break;
+            default:
+                this.setState({HiddenState: 'Hidden'});
+        }
+    }
+
+    handleActiveClick(e, name) {
+        switch(name) {
+            case 'Home':
+                this.setState({activeStates: HOME_TAB_STATE});
+                this.onClickArrow();
+                break;
+            case 'Contact':
+                this.setState({activeStates: CONTACT_TAB_STATE});
+                this.onClickArrow();
+                break;
+            case 'Create':
+                this.setState({activeStates: CREATE_TAB_STATE});
+                this.onClickArrow();
+                break;
+            default:
+                this.setState({activeStates: HOME_TAB_STATE});
+        }
+    }
+
+    render() {
+        return (
+            <div className='left-nav-cluster-container'>
+                <div className='logo-tab'>
+                    <img src='https://via.placeholder.com/40/ffffff.png'/>
+                </div>
+                {this.state.activeStates[0].clickable ? 
+                    <div className={`home-tab__${this.state.HiddenState}__active${this.state.activeStates[0].active}`} onClick={(e) => this.handleActiveClick(e, 'Home')}>Home</div> :
+                    <div className={`home-tab__${this.state.HiddenState}__active${this.state.activeStates[0].active}`} >Home</div>}
+                {this.state.activeStates[1].clickable ? 
+                    <div className={`contact-tab__${this.state.HiddenState}__active${this.state.activeStates[1].active}`}  onClick={(e) => this.handleActiveClick(e, 'Contact')} >About/Contact</div> : 
+                    <div className={`contact-tab__${this.state.HiddenState}__active${this.state.activeStates[1].active}`}>About/Contact</div>}
+                {this.state.activeStates[2].clickable ? 
+                    <div className={`create-post-tab__${this.state.HiddenState}__active${this.state.activeStates[2].active}`}  onClick={(e) => this.handleActiveClick(e, 'Create')} >Create Post</div> : 
+                    <div className={`create-post-tab__${this.state.HiddenState}__active${this.state.activeStates[2].active}`}>Create Post</div>}
+                <div className='arrow-tab' onClick={this.onClickArrow}>Arrow</div>
             </div>
-            <div className={`home-tab__${homeState.hiddenState}__${homeState.activeState}`}>Home</div>
-            <div className='contact-tab'>About/Contact</div>
-            <div className='create-post-tab'>Create Post</div>
-            <div className='arrow-tab'>Arrow</div>
-        </div>
-    )
+        )
+    }
 }
 
 export default LeftNavBarCluster;
