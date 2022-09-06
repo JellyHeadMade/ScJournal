73db4efffc625d12c3e241dd2139d43512d46d4a
JellyHead <JellyHeadM@outlook.com>
2022-09-05 21:17:57 -0400

    tried to add font awesome icon to the image preview but it is not showing up, will need to investige further.

--- src/components/TabFolder/SubTabFolder/imagePrev.js
+++ src/components/TabFolder/SubTabFolder/imagePrev.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
+
 function ImagePrev(props) {
   return (
     <div className={`image-prev${props.index}`}>
       <div className='delete-btn' onClick={props.onClick} title={props.path}>Delete</div>
-      <img className='img-prev' src={props.src} alt='Defualt' />
+      {props.src ? <img src={props.src} /> : <FontAwesomeIcon icon="fa-solid fa-images" />}
+      {/* <FontAwesomeIcon icon="fa-solid fa-images" /> */}
+      {/* <img className='img-prev' src={props.src} alt='Defualt' /> */}
     </div>
   );
 }
