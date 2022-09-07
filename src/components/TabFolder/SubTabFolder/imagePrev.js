65688985aa8825afa55b02fef794a92d24285279
JellyHead <JellyHeadM@outlook.com>
2022-09-07 18:26:46 -0400

    swapped the delete button ternary operator from deciding which div to render, into what className to give it so i can better manage the transition. at the moment it works well with some basic opacity, and font size changes over a 1.2s sample transition.

--- src/components/TabFolder/SubTabFolder/imagePrev.js
+++ src/components/TabFolder/SubTabFolder/imagePrev.js
@@ -6,7 +6,7 @@ import { faImage } from '@fortawesome/free-solid-svg-icons';
 function ImagePrev(props) {
   return (
     <div className={`image-prev${props.index}`}>
-      {props.src ? <div className='delete-btn' onClick={props.onClick} title={props.path}>Delete</div> : null}
+      <div className={props.src ? 'delete-btn-active' : 'delete-btn-notactive'} onClick={props.onClick} title={props.path}>Delete</div>
 
       {props.src ? <img className='thumb-img' src={props.src} /> : 
       <FontAwesomeIcon className='default-icon' icon={faImage} />}
