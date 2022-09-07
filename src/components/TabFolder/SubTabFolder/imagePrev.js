609c7d4d426270949af943bb3aa610cb0e556800
JellyHead <JellyHeadM@outlook.com>
2022-09-06 20:30:08 -0400

    fixed the issue with font awesome icon wasn't appearing. also styled the default, and thumb image. even went and updated the delete button to only render in if there is an image. i will need to change this to a style change so i can just have it transition instead of loading and unloading. i cant put a transition on that.

--- src/components/TabFolder/SubTabFolder/imagePrev.js
+++ src/components/TabFolder/SubTabFolder/imagePrev.js
@@ -6,10 +6,11 @@ import { faImage } from '@fortawesome/free-solid-svg-icons';
 function ImagePrev(props) {
   return (
     <div className={`image-prev${props.index}`}>
-      <div className='delete-btn' onClick={props.onClick} title={props.path}>Delete</div>
-      {props.src ? <img src={props.src} /> : <FontAwesomeIcon icon="fa-solid fa-images" />}
-      {/* <FontAwesomeIcon icon="fa-solid fa-images" /> */}
-      {/* <img className='img-prev' src={props.src} alt='Defualt' /> */}
+      {props.src ? <div className='delete-btn' onClick={props.onClick} title={props.path}>Delete</div> : null}
+
+      {props.src ? <img className='thumb-img' src={props.src} /> : 
+      <FontAwesomeIcon className='default-icon' icon={faImage} />}
+
     </div>
   );
 }
