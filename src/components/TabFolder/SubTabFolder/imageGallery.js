806087abc4ca2e814fd8e6f41e1d05be36463177
JellyHead <JellyHeadM@outlook.com>
2022-10-05 19:46:21 -0400

    added an alt prop in the img div so i can dynamically get the right image link. did realize issue with having a state variable locally to check since it would have to be switched back from redux, other wise it will require two clicks to return the correct link. i think moving this state variable to redux and just have the conditional check the prop. will see how it goes.

--- src/components/TabFolder/SubTabFolder/imageGallery.js
+++ src/components/TabFolder/SubTabFolder/imageGallery.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ImageGallery(props) {
 
+    const [clicked, setClicked] = useState(false);
+
+    const fullImageTrigger = (image) => { // stand in for a call to redux
+        console.log('clicked, here is the image: ', image);
+        console.log('image trigger shows state as ', clicked);
+    }
+
     const onClick = (e) => {
-        console.log('image clicked!');
+        if (!clicked) {
+            setClicked(true);
+            fullImageTrigger(e.target.alt); 
+        } else {
+            setClicked(false);
+        }
+        console.log('onclicked shows state as ', clicked);
     }
 
     return (
         <div className='image-gallery-tab-content'>
             <div onClick={onClick} className='placeholder'>
-                <img className='img' src={props.image1Thumbnail} />
+                <img className='img' alt={props.image1} src={props.image1Thumbnail} />
             </div>
             <div onClick={onClick} className='placeholder'>
-                <img className='img' src={props.image2Thumbnail} />
+                <img className='img' alt={props.image2} src={props.image2Thumbnail} />
             </div>
             <div onClick={onClick} className='placeholder'>
-                <img className='img' src={props.image3Thumbnail} />
+                <img className='img' alt={props.image3} src={props.image3Thumbnail} />
             </div>
         </div>
     )
