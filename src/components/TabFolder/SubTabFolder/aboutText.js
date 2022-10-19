37bb1c9b6b3f9765a5aef4e4518f704e26c302c4
JellyHead <JellyHeadM@outlook.com>
2022-10-18 20:27:20 -0400

    quickly made a compoenent for just the text, so i can style it differently from other body content from the basicPage compoenent.

---
+++ src/components/TabFolder/SubTabFolder/aboutText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutText(props) {
+    return (
+        <div className='about-text-container'>
+            <div className='about-text-body'>This is a fan project that has no official support of cloud imperium games. The stories on this site also do not reflect actual lore of the game. This site is here to allow fans of the projects to have a common place to share their stories and experiences.</div>
+        </div>
+    );
+}
+
+export default AboutText;
