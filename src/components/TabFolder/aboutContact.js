4d11388a4ddd3ea5ba1ec1a6f78a97706e0b409d
JellyHead <JellyHeadM@outlook.com>
2022-10-18 20:23:56 -0400

    added some of the starting text for the about/contact page. the page will still need a form, and i will also want to use a different component to store the actual about text, and another separate compoenent for the form.

--- src/components/TabFolder/aboutContact.js
+++ src/components/TabFolder/aboutContact.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import BasicPage from './SubTabFolder/basicPage';
 
+let aboutPageContent = 'This is a fan project that has no official support \
+of cloud imperium games. The stories on this site also do not reflect actual \
+lore of the game. This site is here to allow fans of the projects to have a \
+common place to share their stories and experiences.';
+
 function AboutContact(props) {
     return (
         <div className='about-contact-page-container'>
-            <BasicPage title='About / Contact' content='placeholdercontent'/>
+            <BasicPage title='About / Contact' content={aboutPageContent}/>
         </div>
     );
 }
