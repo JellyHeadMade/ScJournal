90db70c2912fbd7d797be357fab406dc53d146e3
JellyHead <JellyHeadM@outlook.com>
2022-10-20 21:57:45 -0400

    added a client side service to send an email, as well as the form to go along with it. this is a very basic implemenation, and i dont think i actually need the to_name variable, but you can give your name, who you are addressing, a message, and your email and when you hit submit and it goes though you get an alert saying it succeded and will clear all the fields after. it will alert you saying message failed if it failed. next step is to probably remove the to_name and then add some styles to it.

--- src/components/TabFolder/SubTabFolder/aboutContactForm.js
+++ src/components/TabFolder/SubTabFolder/aboutContactForm.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { send } from 'emailjs-com';
 
 function AboutContactForm(props) {
 
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        to_name: '',
+        message: '',
+        reply_to: '',
+      });
+
+      const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+            'service_9w1vdyj',
+            'template_5f7up5a',
+            toSend,
+            'mQeg9BBrfezQkPMsS'
+          )
+            .then((response) => {
+              console.log('SUCCESS!', response.status, response.text);
+              setToSend({ ...toSend, from_name: '', to_name: '', message: '', reply_to: '' });
+              alert('Message Sent');
+            })
+            .catch((err) => {
+              console.log('FAILED...', err);
+              alert('Message Failed');
+            });
+      };
+    
+      const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+      };
+
     return (
-        <div className='about-contact-form-contact'></div>
+        <div className='about-contact-form-contact'>
+            <form onSubmit={onSubmit}>
+                <input
+                  type='text'
+                  name='from_name'
+                  placeholder='from name'
+                  value={toSend.from_name}
+                  onChange={handleChange}
+                />
+                <input
+                  type='text'
+                  name='to_name'
+                  placeholder='to name'
+                  value={toSend.to_name}
+                  onChange={handleChange}
+                />
+                <input
+                  type='text'
+                  name='message'
+                  placeholder='Your message'
+                  value={toSend.message}
+                  onChange={handleChange}
+                />
+                <input
+                  type='text'
+                  name='reply_to'
+                  placeholder='Your email'
+                  value={toSend.reply_to}
+                  onChange={handleChange}
+                />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
     )
 }
 
