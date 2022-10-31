fc3e4d41ce94425b5474fb678a322e71cd65ae59
JellyHead <JellyHeadM@outlook.com>
2022-10-31 19:01:23 -0400

    put in the main text for the about section of the about/contact page, this is however still subject to change, but would not be overly mad if this shipped with that text in there. also removed the background color for the container, and have also added correct padding and font type, and size of the text

--- src/components/TabFolder/SubTabFolder/aboutText.js
+++ src/components/TabFolder/SubTabFolder/aboutText.js
@@ -3,7 +3,15 @@ import React from 'react';
 function AboutText(props) {
     return (
         <div className='about-text-container'>
-            <div className='about-text-body'>This is a fan project that has no official support of cloud imperium games. The stories on this site also do not reflect actual lore of the game. This site is here to allow fans of the projects to have a common place to share their stories and experiences.</div>
+            <div className='about-text-body'>This is a fan project that has no official support of cloud imperium games. The stories on this site also do not reflect actual lore of the game. This site is here to allow fans of the projects to 
+                have a common place to share their stories and experiences. while at the moment there is no firm rules i think there are a few that should be pretty obvious but will say them to be sure. This site 
+                is for STAR CITIZEN STORIES. so the story should take place or revolve around the Star citizen IP. One should also keep all elements of the story in the IP as well. this is not saying using 
+                terminalogy used in other scifi properties is not allowed, more as it should still relate to something that exsist in Star citizen. for example, if you refer to the Scorpius as the X-Wing i think 
+                that is fine overall, as everyone would know what your talking about. if you start mentioning the force, or characters from other IP's that is grounds for the removal of the story. this is another 
+                that should go without saying but zero tolorance for any type of harrasment of any singular or group of people, as well as no racial, sextual, or IRL political speech. this goes even for those who
+                are appart of a certian race, sextuality, as trying to enforce only the "wrong" people from using that language is a moderation nightmare. lastly remember this is a place to just share stories of 
+                your play experince with star citizen. so lets keep it to that and have a great time! any questions or concerns please feel free to contact me with the form below. Thanks!
+            </div>
         </div>
     );
 }
