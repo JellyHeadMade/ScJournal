7d0e4cbf9558b20e4fc6f86078138a4ea8df4e88
JellyHead <JellyHeadM@outlook.com>
2022-11-09 22:22:40 -0500

    added an onclick function for the arrow to have it flip the chevron arrow on the click, also moved the user icon into the base thing showed, and leave the text of 'login/signup' up for the teranary operator, which will remove it if you are logged in. did come to a design problem where it was never designed clearly how the filters are showed with or without the user mini info showing. will need to get grid-template-rows and coloumns set up next so i can start placing and messing around with them.

--- src/components/TabFolder/SubTabFolder/rightNavBarCluster.js
+++ src/components/TabFolder/SubTabFolder/rightNavBarCluster.js
@@ -7,6 +7,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 
+const FILTER_TAB_STATE = [
+    {name: 'expandedRight', active: true, clickable: false, bgColor: '#4FACEE'}
+];
+
 class RightNavBarCluster extends React.Component{
     constructor(props) {
         super(props);
@@ -16,16 +20,30 @@ class RightNavBarCluster extends React.Component{
             HiddenState: 'Hidden',
         }
         this.userPageClick = this.userPageClick.bind(this);
+        this.onClickArrow = this.onClickArrow.bind(this);
     }
 
     userPageClick = (event) => {
         this.props.setPage('User');
     }
 
+    onClickArrow(e) {
+        switch(this.state.HiddenState) {
+            case 'Hidden': 
+                this.setState({HiddenState: 'notHidden'});
+                break;
+            case 'notHidden':
+                this.setState({HiddenState: 'Hidden'});
+                break;
+            default:
+                this.setState({HiddenState: 'Hidden'});
+        }
+    }
+
     render() {
         return (
             <div className='right-nav-cluster-container'>
-                <div className='profile-tab' onClick={this.userPageClick}>{this.state.loggedIn ? <FontAwesomeIcon icon={faCircleUser} /> : 'Login/Sign Up'}</div>
+                <div className='profile-tab' onClick={this.userPageClick}><FontAwesomeIcon icon={faCircleUser} /> {this.state.loggedIn ? null : 'Login/Sign Up'}</div>
                 {this.state.loggedIn ? <div className='pocket-user-info'>Put user info here</div> : null}
                 <div className='filter-tab'>Filters</div>
                 <div className='arrow-tab' onClick={this.onClickArrow}>
