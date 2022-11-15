a62c2a488f745518667d8c73e26aa7b150f85a1b
JellyHead <JellyHeadM@outlook.com>
2022-11-14 23:54:15 -0500

    added a new 'comment-batch' div as a wrapper to hold both the button and the comment section so i can stack the button under the comments.

--- src/components/TabFolder/SubTabFolder/commentContent.js
+++ src/components/TabFolder/SubTabFolder/commentContent.js
@@ -8,23 +8,25 @@ function CommentContent(props) {
 
     return(
         <div className='comments'>
-            {props.comments.length != 0 ? <div className='comments-container'>
-                {props.comments.map((comments) => 
-                    <div className='comment-wrapper'>
-                        <div className='comment-userimage'>
-                            <img className='comment-userimage__img' src={comments.userImage}></img>
+            <div className='comment-batch'>
+                {props.comments.length != 0 ? <div className='comments-container'>
+                    {props.comments.map((comments) => 
+                        <div className='comment-wrapper'>
+                            <div className='comment-userimage'>
+                                <img className='comment-userimage__img' src={comments.userImage}></img>
+                            </div>
+                            <div className='comment-username'>
+                                <a className='comment-username__name'>{comments.username}</a>
+                            </div>
+                            <div className='comment-post-comment'>
+                                <a className='comment-post-comment__text'>{comments.comment}</a>
+                            </div>
                         </div>
-                        <div className='comment-username'>
-                            <a className='comment-username__name'>{comments.username}</a>
-                        </div>
-                        <div className='comment-post-comment'>
-                            <a className='comment-post-comment__text'>{comments.comment}</a>
-                        </div>
-                    </div>
-                )}  
-            </div> :
-            <div className='comments-container-empty'>No comments Here Yet!</div>}
-            <div className='add-comment-btn' onClick={addCommentOnClick}>Add Comment</div>
+                    )}  
+                </div> :
+                <div className='comments-container-empty'>No comments Here Yet!</div>}
+                <div className='add-comment-btn' onClick={addCommentOnClick}>Add Comment</div>
+            </div>
         </div>
     )
 }
