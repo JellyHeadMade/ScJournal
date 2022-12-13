c96d4c263326548b746e2c00129f354338b5b85b
JellyHead <JellyHeadM@outlook.com>
2022-12-12 20:48:14 -0500

    swapped out the dummy data in the userpage with the basic page component. this is still dummy data but trying to get things prepped for the user API that is live, just need to add routes to the db, then omniauth.

--- src/components/TabFolder/createUserPage.js
+++ src/components/TabFolder/createUserPage.js
@@ -1,14 +1,10 @@
 import React from 'react';
+import BasicPage from '../TabFolder/SubTabFolder/basicPage';
 
 function CreateUserPage(props) {
     return (
         <div className='create-user-page-container'>
-            <div className='create-user-page-header'>
-                <div className='create-user-page-header-text'>Create User</div>
-            </div>
-            <div className='create-user-page-body'>
-                <div className='create-user-page-body-text'>Body</div>
-            </div>
+            <BasicPage title='JellyHeadMade' content='users Stuff!' />
         </div>
     )
 }
